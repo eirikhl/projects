f0f8c1db421bf45daf4dd1fcf105a5a964fc295f
eirikhl <eirikharald@hotmail.com>
2019-12-16 16:49:03 +0100

    Finished minigrep example

--- rust/tutorial/minigrep/src/main.rs
+++ rust/tutorial/minigrep/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        // eprintln! is a function for printing error message to stdout, regardless of normal output
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -17,7 +18,7 @@ fn main() {
     println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
